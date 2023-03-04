0c1bb86041a0c59cce9d1997bb5c995a265e45bb
l-Luna <izzeldeen03@gmail.com>
2023-03-04 20:36:39 +0000

    text highlighting

--- PrismaticHelper/Cutscenes/ParserHooks.cs
+++ PrismaticHelper/Cutscenes/ParserHooks.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Celeste;
 using Celeste.Mod;
+using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
 using Monocle;
 using MonoMod.Cil;
@@ -51,18 +52,26 @@ public static class ParserHooks{
 
 	public static void LoadHooks(){
 		IL.Celeste.FancyText.Parse += ParsePhTriggers;
+		On.Celeste.FancyText.AddWord += TrackPhHighlight;
+		On.Celeste.FancyText.Text.Draw += DrawPhHighlight;
+		
 		On.Celeste.Textbox.ctor_string_Language_Func1Array += AddPhEvents;
+		On.Celeste.Textbox.Removed += TextboxOnRemoved;
+		
 		On.Celeste.Level.SkipCutscene += Level_SkipCutscene;
 		On.Celeste.Level.Pause += LevelOnPause;
-		On.Celeste.Textbox.Removed += TextboxOnRemoved;
 	}
 
 	public static void Unload(){
 		IL.Celeste.FancyText.Parse -= ParsePhTriggers;
+		On.Celeste.FancyText.AddWord -= TrackPhHighlight;
+		On.Celeste.FancyText.Text.Draw -= DrawPhHighlight;
+
 		On.Celeste.Textbox.ctor_string_Language_Func1Array -= AddPhEvents;
+		On.Celeste.Textbox.Removed -= TextboxOnRemoved;
+
 		On.Celeste.Level.SkipCutscene -= Level_SkipCutscene;
 		On.Celeste.Level.Pause -= LevelOnPause;
-		On.Celeste.Textbox.Removed -= TextboxOnRemoved;
 	}
 
 	private static void AddPhEvents(On.Celeste.Textbox.orig_ctor_string_Language_Func1Array orig, Textbox self, string dialog, Language language, Func<System.Collections.IEnumerator>[] events){
@@ -120,18 +129,141 @@ public static class ParserHooks{
 			cursor.Emit(OpCodes.Ldloc_S, il.Method.Body.Variables[7]); // s
 			cursor.Emit(OpCodes.Ldloc_S, il.Method.Body.Variables[8]); // stringList
 			cursor.EmitDelegate<Action<FancyText, string, List<string>>>((text, s, vals) => {
-				List<FancyText.Node> nodes = new DynamicData(text).Get<FancyText.Text>("group").Nodes;
-				if(s.Equals("ph_trigger") || s.Equals("&ph_trigger") || s.Equals("~ph_trigger"))
-					nodes.Add(new PhTrigger(vals, s.StartsWith("&"), s.StartsWith("~")));
-				if(s.Equals("ph_on_skip"))
-					nodes.Add(new PhRunOnSkip(vals));
+				var parserData = new DynamicData(text);
+				FancyText.Text group = parserData.Get<FancyText.Text>("group");
+				List<FancyText.Node> nodes = group.Nodes;
+				switch(s){
+					case "ph_trigger":
+					case "&ph_trigger":
+					case "~ph_trigger":
+						nodes.Add(new PhTrigger(vals, s.StartsWith("&"), s.StartsWith("~")));
+						break;
+					case "ph_on_skip":
+						nodes.Add(new PhRunOnSkip(vals));
+						break;
+					case "ph_highlight":
+						parserData.Set("PrismaticHelper:current_highlight", vals.Count == 0 ? null : Calc.HexToColor(vals[0]));
+						DynamicData.For(group).Set("PrismaticHelper:has_highlight", true);
+						break;
+				}
 			});
 		}
 	}
 	
+	private static void TrackPhHighlight(On.Celeste.FancyText.orig_AddWord orig, FancyText self, string word){
+		orig(self, word);
+		// skip backslashes, skip space preceded by newlines (how?)
+		var parserData = new DynamicData(self);
+		parserData.TryGet("PrismaticHelper:current_highlight", out Color? h);
+		if(h != null){
+			List<FancyText.Node> nodes = parserData.Get<FancyText.Text>("group").Nodes;
+			int idx = nodes.Count;
+			for(int i = 0; i < word.Replace("\\", "").Length; i++){
+				idx--;
+				if(idx < 0)
+					break;
+
+				if(nodes[idx] is FancyText.Char c)
+					DynamicData.For(c).Set("PrismaticHelper:highlight", h);
+				else i--; // triggers and stuff don't count towards our total
+			}
+		}
+	}
+	
+	private static void DrawPhHighlight(On.Celeste.FancyText.Text.orig_Draw orig, FancyText.Text self, Vector2 position, Vector2 justify, Vector2 scale, float alpha, int start, int end){
+		var selfData = DynamicData.For(self);
+		if(selfData.TryGet("PrismaticHelper:has_highlight", out bool? b) && b == true){
+			// find each consecutive group of highlights and draw
+			Color? lastColour = null;
+			FancyText.Char openingChar = null, closingChar = null;
+			for(var idx = start; idx < self.Nodes.Count; idx++){
+				var node = self.Nodes[idx];
+				if(node is FancyText.Char c){
+					DynamicData.For(c).TryGet("PrismaticHelper:highlight", out Color? highlight);
+					// highlight is null -> draw? and reset
+					// highlight is new and no previous highlight -> start one
+					// highlight is same as prev -> expand prev
+					// highlight is new and prev exists -> draw and start new
+					if(highlight == null){
+						if(lastColour != null && openingChar != null && closingChar != null)
+							DrawHighlightOn(lastColour.Value, self, start, openingChar, closingChar, position, justify, scale, alpha);
+						lastColour = null;
+						openingChar = closingChar = null;
+					}else if(lastColour == null){
+						lastColour = highlight;
+						openingChar = closingChar = c;
+					}else if(lastColour == highlight){
+						closingChar = c;
+					}else{
+						DrawHighlightOn(lastColour.Value, self, start, openingChar, closingChar, position, justify, scale, alpha);
+						lastColour = highlight;
+						openingChar = closingChar = c;
+					}
+				}else if(node is FancyText.NewLine or FancyText.NewPage)
+					// hit newline -> draw?
+					if(lastColour != null && openingChar != null && closingChar != null){
+						DrawHighlightOn(lastColour.Value, self, start, openingChar, closingChar, position, justify, scale, alpha);
+						lastColour = null;
+						openingChar = closingChar = null;
+					}
+			}
+
+			// hit end -> draw?
+			if(lastColour != null && openingChar != null && closingChar != null)
+				DrawHighlightOn(lastColour.Value, self, start, openingChar, closingChar, position, justify, scale, alpha);
+		}
+
+		orig(self, position, justify, scale, alpha, start, end);
+	}
+
+	private static void DrawHighlightOn(Color highlightColour, FancyText.Text self, int pageStart, FancyText.Char opening, FancyText.Char closing, Vector2 pos, Vector2 justify, Vector2 scale, float alpha){
+		// see FancyText::Draw for reference
+		if(closing.Fade >= 0.1){
+			Vector2 exactPos = pos;
+			// figure out the text's actual starting position
+			PixelFontSize basePfSize = self.Font.Get(self.BaseSize);
+			float maxScale = 0;
+			float pageHeight = 0;
+			int maxWidth = 0;
+			int newlinesBefore = 0;
+			bool reachedOpening = false;
+			for(int index = pageStart; index < self.Nodes.Count; ++index){
+				if(self.Nodes[index] is FancyText.NewLine){
+					if(maxScale == 0)
+						maxScale = 1;
+					pageHeight += maxScale;
+					maxScale = 0;
+					if(!reachedOpening)
+						newlinesBefore++;
+				}else if(self.Nodes[index] is FancyText.Char c){
+					maxWidth = Math.Max(maxWidth, (int)c.LineWidth);
+					maxScale = Math.Max(maxScale, c.Scale);
+					if(c == opening)
+						reachedOpening = true;
+				}else if(self.Nodes[index] is FancyText.NewPage)
+					break;
+			}
+			exactPos -= justify * new Vector2(maxWidth, (maxScale + pageHeight) * basePfSize.LineHeight) * scale;
+			exactPos.X += opening.Position * scale.X;
+			// and it's actual size
+			float localMaxScale = Math.Max(opening.Scale, closing.Scale);
+			PixelFontSize exactPfSize = self.Font.Get(self.BaseSize * Math.Max(scale.X, scale.Y) * localMaxScale);
+			PixelFontCharacter closingPfChar = exactPfSize.Get(closing.Character);
+			var width = closing.Position - opening.Position + closingPfChar.XAdvance;
+			var height = basePfSize.LineHeight * scale.Y;
+			exactPos.Y += exactPfSize.LineHeight * newlinesBefore;
+			// give it some breathing room
+			const float padding = 10;
+			exactPos.X -= padding;
+			width += padding * 2;
+			
+			Draw.Rect(exactPos, width * closing.Fade, height, highlightColour * closing.Fade * .5f);
+		}
+	}
+
 	private static void LevelOnPause(On.Celeste.Level.orig_Pause orig, Level self, int startindex, bool minimal, bool quickreset){
 		bool wasInCutscene = self.InCutscene, couldRetry = self.CanRetry;
-		if(new DynamicData(self).TryGet("PrismaticHelper:force_unskippable", out bool? noSkip) && noSkip == true){
+		if(DynamicData.For(self).TryGet("PrismaticHelper:force_unskippable", out bool? noSkip) && noSkip == true){
 			self.InCutscene = false;
 			self.CanRetry = false;
 		}
