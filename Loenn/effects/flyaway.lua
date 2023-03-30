6e6d1137d8bae407850ef183b052d523ad35a6de
l-Luna <izzeldeen03@gmail.com>
2023-03-30 17:03:16 +0100

    clean up Flyaway

--- Loenn/effects/flyaway.lua
+++ Loenn/effects/flyaway.lua
@@ -2,11 +2,22 @@
 
 flyaway.name = "PrismaticHelper/Flyaway"
 flyaway.canBackground = true
-flyaway.canForeground = false
+flyaway.canForeground = true
 
 flyaway.defaultData = {
-    textures = ""
+    textures = "",
+    
+    limit = 170,
+    chance = 0.5,
+    speed = 1,
+    spawnDistance = 1.5,
+    baseScale = 1,
+    bonusScale = 0.5,
+    baseAlpha = 1,
+    
+    canFlipX = true,
+    canFlipY = true,
+    scaleAlpha = true
 }
 
-return nil
---return flyaway
+return flyaway
--- PrismaticHelper/Effects/Flyaway.cs
+++ PrismaticHelper/Effects/Flyaway.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Celeste;
+using Celeste.Mod;
 using Celeste.Mod.Backdrops;
 using Microsoft.Xna.Framework;
 using Monocle;
@@ -11,12 +12,16 @@ namespace PrismaticHelper.Effects;
 public class Flyaway : Backdrop{
 
 	private static readonly Vector2 Centre = new(160, 90);
+
+	public readonly int Limit;
+	public readonly float Chance, SpawnDistance, BaseScale, BonusScale, BaseAlpha;
+	public new readonly float Speed;
+	public readonly bool CanFlipX, CanFlipY, ScaleAlpha;
 	
-	private readonly List<Prop> Props = new();
+	protected readonly List<Prop> props = new();
+	protected readonly List<MTexture> propSprites;
 	
-	private readonly List<MTexture> propSprites;
-	
-	private class Prop{
+	protected class Prop{
 		public MTexture texture;
 		public Vector2 position;
 		public float scale;
@@ -24,36 +29,48 @@ public class Flyaway : Backdrop{
 	}
 
 	public Flyaway(BinaryPacker.Element e){
+		Limit = e.AttrInt("limit", 170);
+		
+		Chance = e.AttrFloat("chance", 0.5f);
+		Speed = e.AttrFloat("speed", 1);
+		SpawnDistance = e.AttrFloat("spawnDistance", 1.5f);
+		BaseScale = e.AttrFloat("baseScale", 1f);
+		BonusScale = e.AttrFloat("bonusScale", 0.5f);
+		BaseAlpha = e.AttrFloat("baseAlpha", 1);
+		
+		CanFlipX = e.AttrBool("canFlipX", true);
+		CanFlipY = e.AttrBool("canFlipY", true);
+		ScaleAlpha = e.AttrBool("scaleAlpha", true);
+		
 		propSprites = GFX.Game.GetAtlasSubtextures(e.Attr("textures"));
 	}
 
 	public override void Update(Scene scene){
 		base.Update(scene);
-		foreach(var prop in Props.ToArray()){
+		foreach(var prop in props.ToArray()){
 			if((prop.position - Centre).LengthSquared() < 5 * 5)
-				Props.Remove(prop);
-			prop.position += (Centre - prop.position) * Engine.DeltaTime;
+				props.Remove(prop);
+			prop.position += (Centre - prop.position) * Engine.DeltaTime * Speed;
 		}
 
-		if(Props.Count < 170)
-			if(Calc.Random.Chance(0.5f))
-			//for(int i = 0; i < Calc.Random.Next(1, 2); i++)
-				Props.Add(new Prop{
+		if(props.Count < Limit)
+			if(Calc.Random.Chance(Chance))
+				props.Add(new Prop{
 					texture = Calc.Random.Choose(propSprites),
-					position = (Centre.Rotate(Calc.Random.NextFloat((float)(Math.PI * 2))) * 1.5f) + Centre,
-					scale = 0.2f + Calc.Random.NextFloat(0.1f),
-					flipX = Calc.Random.Chance(0.5f),
-					flipY = Calc.Random.Chance(0.5f)
+					position = (Centre.Rotate(Calc.Random.NextFloat((float)(Math.PI * 2))) * SpawnDistance) + Centre,
+					scale = BaseScale + Calc.Random.NextFloat(BonusScale),
+					flipX = CanFlipX && Calc.Random.Chance(0.5f),
+					flipY = CanFlipY && Calc.Random.Chance(0.5f)
 				});
 	}
 
 	public override void Render(Scene scene){
 		base.Render(scene);
-		foreach(var prop in Props){
+		foreach(var prop in props){
 			var scale = prop.scale * ((Centre - prop.position).Length() / Centre.Length());
 			prop.texture.DrawCentered(
 				prop.position,
-				Color.White * FadeAlphaMultiplier * scale * 1.4f,
+				Color.White * FadeAlphaMultiplier * (ScaleAlpha ? scale : 1) * BaseAlpha,
 				new Vector2(scale * (prop.flipX ? -1 : 1), scale * (prop.flipY ? -1 : 1))
 			);
 		}
