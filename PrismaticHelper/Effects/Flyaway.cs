7f387c80c6d08b7bd82016b62fe42ef1a5fadfcb
l-Luna <izzeldeen03@gmail.com>
2023-03-30 01:21:49 +0100

    make flyaway actually good

--- PrismaticHelper/Effects/Flyaway.cs
+++ PrismaticHelper/Effects/Flyaway.cs
@@ -20,6 +20,7 @@ public class Flyaway : Backdrop{
 		public MTexture texture;
 		public Vector2 position;
 		public float scale;
+		public bool flipX, flipY;
 	}
 
 	public Flyaway(BinaryPacker.Element e){
@@ -29,21 +30,32 @@ public class Flyaway : Backdrop{
 	public override void Update(Scene scene){
 		base.Update(scene);
 		foreach(var prop in Props.ToArray()){
-			if((prop.position - Centre).LengthSquared() < 20 * 20)
+			if((prop.position - Centre).LengthSquared() < 5 * 5)
 				Props.Remove(prop);
-			prop.position += (Centre - prop.position) * Engine.DeltaTime * 6;
+			prop.position += (Centre - prop.position) * Engine.DeltaTime;
 		}
-		if(Props.Count < 150 && Calc.Random.Chance(0.8f))
-			Props.Add(new Prop{
-				texture = Calc.Random.Choose(propSprites),
-				position = Centre.Rotate(Calc.Random.NextFloat((float)(Math.PI * 2))) + Centre,
-				scale = 0.2f + Calc.Random.NextFloat(0.1f)
-			});
+
+		if(Props.Count < 170)
+			if(Calc.Random.Chance(0.5f))
+			//for(int i = 0; i < Calc.Random.Next(1, 2); i++)
+				Props.Add(new Prop{
+					texture = Calc.Random.Choose(propSprites),
+					position = (Centre.Rotate(Calc.Random.NextFloat((float)(Math.PI * 2))) * 1.5f) + Centre,
+					scale = 0.2f + Calc.Random.NextFloat(0.1f),
+					flipX = Calc.Random.Chance(0.5f),
+					flipY = Calc.Random.Chance(0.5f)
+				});
 	}
 
 	public override void Render(Scene scene){
 		base.Render(scene);
-		foreach(var prop in Props)
-			prop.texture.DrawCentered(prop.position, Color.White, (float)(prop.scale * Math.Sqrt((Centre - prop.position).Length() / Centre.Length())));
+		foreach(var prop in Props){
+			var scale = prop.scale * ((Centre - prop.position).Length() / Centre.Length());
+			prop.texture.DrawCentered(
+				prop.position,
+				Color.White * FadeAlphaMultiplier * scale * 1.4f,
+				new Vector2(scale * (prop.flipX ? -1 : 1), scale * (prop.flipY ? -1 : 1))
+			);
+		}
 	}
 }
