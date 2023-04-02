70acbd144d0ca4c532c5633bb27521b40f9dc07a
l-Luna <izzeldeen03@gmail.com>
2023-04-02 17:41:04 +0100

    fix map reloading with complex stylegrounds panels

--- PrismaticHelper/Entities/Panels/StylegroundsPanelRender.cs
+++ PrismaticHelper/Entities/Panels/StylegroundsPanelRender.cs
@@ -30,6 +30,29 @@ public static class StylegroundsPanelRenderer{
 					// TODO: also check if visible in any rooms that are part of a room transition
 					if(!lvl.Paused && !IsVisible(bg, lvl, lvl.Session.Level, !wasForceVisible))
 						bg.Update(level);
+					bg.Visible = wasVisible;
+					bg.ForceVisible = wasForceVisible;
+				}
+		}
+	}
+	
+	public static void BeforeRenderStylegroundPanels(bool fg, Scene level, BackdropRenderer renderer){
+		List<IGrouping<string, AbstractPanel>> toUpdate = level.Tracker
+			.GetEntities<StylegroundsPanel>()
+			.Cast<AbstractPanel>()
+			.Where(it => it.Foreground == fg)
+			.Concat(level.Tracker.GetEntities<WorldPanel>().Cast<AbstractPanel>())
+			.GroupBy(it => it.RoomName)
+			.ToList();
+
+		foreach(var item in toUpdate){
+			var room = item.Key;
+			Level lvl = level as Level;
+			foreach(Backdrop bg in renderer.Backdrops)
+				if(IsVisible(bg, lvl, room) && !bg.Visible){
+					bool wasVisible = bg.Visible, wasForceVisible = bg.ForceVisible;
+					bg.Visible = true;
+					bg.ForceVisible = true;
 					bg.BeforeRender(level);
 					bg.Visible = wasVisible;
 					bg.ForceVisible = wasForceVisible;
@@ -38,7 +61,6 @@ public static class StylegroundsPanelRenderer{
 	}
 
 	public static void RenderStylegroundsPanels(bool fg, Scene level, BackdropRenderer renderer){
-
 		Camera camera = (level as Level).Camera;
 		// find all of the panels we want to fill
 		List<IGrouping<string, StylegroundsPanel>> toRender = level.Entities
@@ -108,11 +130,13 @@ public static class StylegroundsPanelRenderer{
 	public static void Load(){
 		On.Celeste.BackdropRenderer.Render += OnBackdropRendererRender;
 		On.Celeste.BackdropRenderer.Update += OnBackdropRendererUpdate;
+		On.Celeste.BackdropRenderer.BeforeRender += OnBackdropRendererBeforeRender;
 	}
 
 	public static void Unload(){
 		On.Celeste.BackdropRenderer.Render -= OnBackdropRendererRender;
 		On.Celeste.BackdropRenderer.Update -= OnBackdropRendererUpdate;
+		On.Celeste.BackdropRenderer.BeforeRender -= OnBackdropRendererBeforeRender;
 	}
 
 	public static void OnBackdropRendererRender(On.Celeste.BackdropRenderer.orig_Render orig, BackdropRenderer self, Scene scene){
@@ -131,6 +155,15 @@ public static class StylegroundsPanelRenderer{
 				UpdateStylegroundPanels(true, level, self);
 			else if(self == level.Background)
 				UpdateStylegroundPanels(false, level, self);
+	}
+	
+	public static void OnBackdropRendererBeforeRender(On.Celeste.BackdropRenderer.orig_BeforeRender orig, BackdropRenderer self, Scene scene){
+		orig(self, scene);
+		if(scene is Level level)
+			if(self == level.Foreground)
+				BeforeRenderStylegroundPanels(true, level, self);
+			else if(self == level.Background)
+				BeforeRenderStylegroundPanels(false, level, self);
 	}
 
 	private static bool IsVisible(Backdrop styleground, Level level, string room, bool ignoreFV = false){
