7abcb796a3b77c8d1f674ac99e60efca0622f191
l-Luna <izzeldeen03@gmail.com>
2023-04-19 19:41:02 +0100

    heatwave transition controller

---
+++ Loenn/entities/heatwave_transition_controller.lua
@@ -0,0 +1,9 @@
+﻿local heatwave_controller = {}
+
+heatwave_controller.name = "PrismaticHelper/HeatwaveTransitionController"
+heatwave_controller.texture = "PrismaticHelper/icons/heatwave_transition_controller"
+heatwave_controller.placements = {
+    name = "controller"
+}
+
+return heatwave_controller
---
+++ PrismaticHelper/Entities/Objects/HeatwaveTransitionController.cs
@@ -0,0 +1,23 @@
+﻿using Celeste;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+
+namespace PrismaticHelper.Entities.Objects;
+
+[CustomEntity("PrismaticHelper/HeatwaveTransitionController")]
+public class HeatwaveTransitionController : Entity{
+
+	public HeatwaveTransitionController(EntityData data, Vector2 offset) : base(data.Position + offset){}
+	public HeatwaveTransitionController(){}
+	
+	public override void Update(){
+		base.Update();
+		Level l = SceneAs<Level>();
+		foreach(var heatwave in l.Foreground.GetEach<HeatWave>())
+			DynamicData.For(heatwave).Set("wasShow", false);
+		foreach(var heatwave in l.Background.GetEach<HeatWave>())
+			DynamicData.For(heatwave).Set("wasShow", false);
+	}
+}
