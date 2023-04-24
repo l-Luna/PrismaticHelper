90957696cf8f5383ead15104bc6d04868aed0bb0
l-Luna <izzeldeen03@gmail.com>
2023-04-24 19:55:05 +0100

    custom hanging lamp

---
+++ Loenn/entities/custom_hanging_lamp.lua
@@ -0,0 +1,69 @@
+﻿local drawableSprite = require("structs.drawable_sprite")
+local utils = require("utils")
+
+local customHangingLamp = {}
+
+customHangingLamp.name = "PrismaticHelper/CustomHangingLamp"
+customHangingLamp.depth = 2000
+customHangingLamp.minimumSize = {0, 16}
+customHangingLamp.canResize = {false, true}
+customHangingLamp.placements = {
+    name = "hanging_lamp",
+    data = {
+        height = 16,
+        attached = false,
+        sprite = "",
+        --glowColour = "ffffff",
+        glowStartRadius = 24,
+        glowEndRadius = 48
+    }
+}
+
+customHangingLamp.fieldInformation = {
+    glowColour = {
+        fieldType = "color"
+    }
+}
+
+-- Manual offsets and justifications of the sprites
+function customHangingLamp.sprite(room, entity)
+    local sprites = {}
+    local height = math.max(entity.height or 0, 16)
+    local texture = (entity.sprite and string.len(entity.sprite) > 0 and entity.sprite) or "objects/hanginglamp"
+
+    local topSprite = drawableSprite.fromTexture(texture, entity)
+
+    topSprite:setJustification(0, 0)
+    topSprite:setOffset(0, 0)
+    topSprite:useRelativeQuad(0, 0, 8, 8)
+
+    table.insert(sprites, topSprite)
+
+    for i = 0, height - 16, 8 do
+        local middleSprite = drawableSprite.fromTexture(texture, entity)
+
+        middleSprite:setJustification(0, 0)
+        middleSprite:setOffset(0, 0)
+        middleSprite:addPosition(0, i)
+        middleSprite:useRelativeQuad(0, 8, 8, 8)
+
+        table.insert(sprites, middleSprite)
+    end
+
+    local bottomSprite = drawableSprite.fromTexture(texture, entity)
+
+    bottomSprite:setJustification(0, 0)
+    bottomSprite:setOffset(0, 0)
+    bottomSprite:addPosition(0, height - 8)
+    bottomSprite:useRelativeQuad(0, 16, 8, 8)
+
+    table.insert(sprites, bottomSprite)
+
+    return sprites
+end
+
+function customHangingLamp.selection(room, entity)
+    return utils.rectangle(entity.x, entity.y, 8, math.max(entity.height, 16))
+end
+
+return customHangingLamp
---
+++ PrismaticHelper/Entities/Objects/CustomHangingLamp.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Celeste;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+
+namespace PrismaticHelper.Entities.Objects;
+
+[CustomEntity("PrismaticHelper/CustomHangingLamp")]
+public class CustomHangingLamp : HangingLamp{
+
+	private readonly DynamicData selfData;
+	
+	public CustomHangingLamp(EntityData e, Vector2 position) : base(e, e.Position + position){
+		selfData = new DynamicData(this);
+		
+		string sprite = e.Attr("sprite");
+		if(!string.IsNullOrWhiteSpace(sprite)){
+			var images = selfData.Get<List<Image>>("images");
+			foreach(var image in images)
+				image.RemoveSelf();
+			images.Clear();
+
+			// ... vanilla copy from HangingLamp ctor
+			MTexture texture = GFX.Game[sprite];
+			for(int idx = 0; idx < Length - 8; idx += 8){
+				Image chainImg;
+				Add(chainImg = new Image(texture.GetSubtexture(0, 8, 8, 8)));
+				chainImg.Origin.X = 4f;
+				chainImg.Origin.Y = -idx;
+				images.Add(chainImg);
+			}
+
+			Image baseImg;
+			Add(baseImg = new Image(texture.GetSubtexture(0, 0, 8, 8)));
+			baseImg.Origin.X = 4f;
+			Image headImg;
+			Add(headImg = new Image(texture.GetSubtexture(0, 16, 8, 8)));
+			headImg.Origin.X = 4f;
+			headImg.Origin.Y = -(Length - 8);
+			images.Add(headImg);
+		}
+
+		bool attached = e.Bool("attached");
+		if(attached)
+			Add(new StaticMover{
+				SolidChecker = solid => CollideCheckOutside(solid, Position - Vector2.UnitY)
+			});
+
+		var vertexLight = selfData.Get<VertexLight>("light");
+		// TODO: doesn't work
+		
+		Color glowColour = e.HexColor("glowColour", Color.White);
+		if(glowColour != Color.White){
+			vertexLight.Color = glowColour;
+			vertexLight.Dirty = true;
+		}
+
+		vertexLight.StartRadius = e.Float("glowStartRadius", 24);
+		vertexLight.EndRadius = e.Float("glowEndRadius", 48);
+		selfData.Get<BloomPoint>("bloom").Radius = e.Float("glowEndRadius", 48);
+	}
+}
