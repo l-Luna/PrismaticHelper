30511fa666a93278378264b12f55e7181a95c470
l-Luna <izzeldeen03@gmail.com>
2023-06-26 22:54:05 +0100

    hide speedberry (for now)

--- Loenn/entities/speed_berry.lua
+++ Loenn/entities/speed_berry.lua
@@ -1,11 +1,12 @@
-﻿return {
-    name = "PrismaticHelper/SpeedBerry",
-    depth = -100,
-    placements = {
-        name = "berry",
-        data = {
-            time = 60
-        }
-    },
-    texture = "collectables/strawberry/idle00"
-}
+﻿--return {
+--    name = "PrismaticHelper/SpeedBerry",
+--    depth = -100,
+--    placements = {
+--        name = "berry",
+--        data = {
+--            time = 60
+--        }
+--    },
+--    texture = "collectables/strawberry/idle00"
+--}
+return nil
