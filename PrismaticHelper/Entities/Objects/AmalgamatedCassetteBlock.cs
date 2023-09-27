cffc5d99e0c362e7c8988ff1868bdc8a3f48816c
l-Luna <izzeldeen03@gmail.com>
2023-09-27 11:57:40 +0100

    make amalgamated cassette blocks use `NinePatch` and XML sprites

--- PrismaticHelper/Entities/Objects/AmalgamatedCassetteBlock.cs
+++ PrismaticHelper/Entities/Objects/AmalgamatedCassetteBlock.cs
@@ -19,12 +19,8 @@ public class AmalgamatedCassetteBlock : Solid{
 	protected Wiggler scaleWiggler;
 	protected bool activated;
 	protected BoxSide side;
-	private int blockHeight = 2;
-
-	// wehhhh
-	private List<Image> pressed = new();
-	private List<Image> solid = new();
-	private List<Image> all = new();
+	protected int blockHeight = 0;
+	protected Sprite sprite;
 
 	public AmalgamatedCassetteBlock(EntityData data, Vector2 offset) : base(data.Position + offset, data.Width, data.Height, false){
 		Collidable = false;
@@ -53,69 +49,11 @@ public class AmalgamatedCassetteBlock : Solid{
 		base.Awake(scene);
 		scene.Add(side = new BoxSide(this));
 
-		// weh
-		for(float left = Left; left < (double)Right; left += 8f)
-			for(float top = Top; top < (double)Bottom; top += 8f){
-				bool flag1 = CheckForSame(left - 8f, top);
-				bool flag2 = CheckForSame(left + 8f, top);
-				bool flag3 = CheckForSame(left, top - 8f);
-				bool flag4 = CheckForSame(left, top + 8f);
-				if(flag1 & flag2 & flag3 & flag4)
-					if(!CheckForSame(left + 8f, top - 8f))
-						SetImage(left, top, 3, 0);
-					else if(!CheckForSame(left - 8f, top - 8f))
-						SetImage(left, top, 3, 1);
-					else if(!CheckForSame(left + 8f, top + 8f))
-						SetImage(left, top, 3, 2);
-					else if(!CheckForSame(left - 8f, top + 8f))
-						SetImage(left, top, 3, 3);
-					else
-						SetImage(left, top, 1, 1);
-				else if(((!(flag1 & flag2) ? 0 : (!flag3 ? 1 : 0)) & (flag4 ? 1 : 0)) != 0)
-					SetImage(left, top, 1, 0);
-				else if(flag1 & flag2 & flag3 && !flag4)
-					SetImage(left, top, 1, 2);
-				else if(((!flag1 ? 0 : (!flag2 ? 1 : 0)) & (flag3 ? 1 : 0) & (flag4 ? 1 : 0)) != 0)
-					SetImage(left, top, 2, 1);
-				else if(!flag1 & flag2 & flag3 & flag4)
-					SetImage(left, top, 0, 1);
-				else if(((!flag1 || flag2 ? 0 : (!flag3 ? 1 : 0)) & (flag4 ? 1 : 0)) != 0)
-					SetImage(left, top, 2, 0);
-				else if(((!(!flag1 & flag2) ? 0 : (!flag3 ? 1 : 0)) & (flag4 ? 1 : 0)) != 0)
-					SetImage(left, top, 0, 0);
-				else if(((!flag1 ? 0 : (!flag2 ? 1 : 0)) & (flag3 ? 1 : 0)) != 0 && !flag4)
-					SetImage(left, top, 2, 2);
-				else if(!flag1 & flag2 & flag3 && !flag4)
-					SetImage(left, top, 0, 2);
-			}
+		Add(sprite = GFX.SpriteBank.Create("PrismaticHelper_amalgamated_cassette_block"));
+		NinePatch.CreateConnectedNinepatch(this, sprite, NinePatch.TileSpec.CassetteLike, block =>
+			block.Indices.Intersect(Indices).Count() == Indices.Count);
 	}
 
-	// wehhh
-	private bool CheckForSame(float x, float y){
-		return Scene.Tracker.GetEntities<AmalgamatedCassetteBlock>()
-			.OfType<AmalgamatedCassetteBlock>()
-			.Any(entity => entity.Indices.Intersect(Indices).Count() == Indices.Count
-			               && entity.Collider.Collide(new Rectangle((int)x, (int)y, 8, 8)));
-	}
-
-	private void SetImage(float x, float y, int tx, int ty){
-		List<MTexture> atlasSubtextures = GFX.Game.GetAtlasSubtextures("objects/cassetteblock/pressed");
-		pressed.Add(CreateImage(x, y, tx, ty, atlasSubtextures[1 % atlasSubtextures.Count]));
-		solid.Add(CreateImage(x, y, tx, ty, GFX.Game["objects/cassetteblock/solid"]));
-	}
-
-	private Image CreateImage(float x, float y, int tx, int ty, MTexture tex){
-		Vector2 offset = new Vector2(x - X, y - Y);
-		Image image = new Image(tex.GetSubtexture(tx * 8, ty * 8, 8, 8));
-		Vector2 origin = -Position;
-		image.Origin = origin - offset;
-		image.Position = origin;
-		Add(image);
-		all.Add(image);
-		return image;
-	}
-
-	// unweh
 	public override void Render(){
 		Draw.SpriteBatch.End();
 		Engine.Graphics.GraphicsDevice.SetRenderTarget(Stencils.MaskRenderTarget);
---
+++ PrismaticHelper/Entities/Objects/ForceRoomTransitionTrigger.cs
@@ -0,0 +1,13 @@
+﻿using Celeste;
+using Microsoft.Xna.Framework;
+
+namespace PrismaticHelper.Entities.Objects;
+
+public class ForceRoomTransitionTrigger : Trigger{
+	
+	public ForceRoomTransitionTrigger(EntityData data, Vector2 offset) : base(data, offset){}
+
+	public override void OnEnter(Player player){
+		base.OnEnter(player);
+	}
+}
