102b95d920a5105a79c20043d62ee265ca74f3f8
l-Luna <izzeldeen03@gmail.com>
2023-10-13 12:34:23 +0100

    amalgamated cassette block plugin

---
+++ Loenn/entities/amalgamated_cassette_block.lua
@@ -0,0 +1,20 @@
+﻿local utils = require("utils")
+
+local amalgam = {}
+
+amalgam.name = "PrismaticHelper/AmalgamatedCassetteBlock"
+amalgam.minimumSize = {16, 16}
+amalgam.placements = {
+    name = "amalgam",
+    data = {
+        indices = "0,1,2,3",
+        colors = "49aaf0,f049be,fcdc3a,38e04e"
+    }
+}
+
+function amalgam.fillColor(_, entity)
+    local colors = (entity.colors and string.len(entity.colors) >= 6 and entity.colors) or "000000"
+    return utils.getColor(string.sub(colors, 1, 6))
+end 
+
+return amalgam
